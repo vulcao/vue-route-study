abbb9bbb975a987afdf6ad736f293095d282f21e
vulcao <vulcao@gmail.com>
2021-10-29 00:04:38 -0300

    aula inicial - cap2-item6

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,24 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/brazil",
+    name: "brazil",
+    component: () => import(/* webpackChunkName: "brazil" */ "../views/Brazil"),
+  },
+  {
+    path: "/hawaii",
+    name: "hawaii",
+    component: () => import(/* webpackChunkName: "hawaii" */ "../views/Hawaii"),
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: () => import(/* webpackChunkName: "janaica" */ "../views/Jamaica"),
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: () => import(/* webpackChunkName: "panama" */ "../views/Panama"),
   },
 ];
 
---
+++ src/views/Panama.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2>Panama</h2>
+
+    </div>
+</template>
